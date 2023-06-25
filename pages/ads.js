8ea6849709cb276a880df996186916cf08636fdb
Rizky Ananta <anantaristik@gmail.com>
2023-06-26 01:30:29 +0700

    updated revision

--- pages/ads.js
+++ pages/ads.js
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import React from 'react';
 import Hero from '../components/LandingAds/Hero/app';
+import PageTwo from '../components/LandingAds/PageTwo/app';
 
 const Contact = () => {
 
   return (
     <Box color="black" background="#f2f2f2" minHeight="100vh">
       <Hero />
+      <PageTwo />
     </Box>
   );
 };
