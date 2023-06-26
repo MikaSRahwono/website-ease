0377015cb2189c2c5919d1508a2f62e2436c5483
Rizky Ananta <anantaristik@gmail.com>
2023-06-26 12:48:02 +0700

    fixing for deploy

--- components/LandingAds/ApaYangBerbeda/app.jsx
+++ components/LandingAds/ApaYangBerbeda/app.jsx
@@ -4,52 +4,44 @@ import { berbedas } from "./_data";
 export default function Keuntungan() {
   return (
     <Box>
-        <Box h="80vh" position="relative">
-          <Box
-            position="absolute"
-            top="0"
-            left="0"
-            right="0"
-            bottom="0"
-          />
+      <Box h="80vh" position="relative">
+        <Box position="absolute" top="0" left="0" right="0" bottom="0" />
 
-          <VStack
-            spacing={4}
-            align="left"
-            w={{ base: '60vw', lg: '60vw'}}
-            h={{ base: '20vh'}}
-            p="2rem"
-            position="absolute"
-            top="20%"
-            left="50%"
-            transform="translate(-50%, -50%)"
-            zIndex="1"
-            textAlign="left"
+        <VStack
+          spacing={4}
+          align="left"
+          w={{ base: '60vw', lg: '60vw'}}
+          h={{ base: '20vh'}}
+          p="2rem"
+          position="absolute"
+          top="20%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          zIndex="1"
+          textAlign="left"
+        >
+          <Heading
+            fontFamily="Aileron"
+            fontSize={{ base: "3xl", md: "5xl", lg: "3xl" }}
+            paddingBottom="2rem"
           >
-            <Heading
-              fontFamily="Aileron"
-              fontSize={{ base: "3xl", md: "5xl", lg: "3xl" }}
-              paddingBottom="2rem"
-            >
-              Apa yang berbeda dari Jasa Foto Produk di Ease Your Needs?
-            </Heading>
+            Apa yang berbeda dari Jasa Foto Produk di Ease Your Needs?
+          </Heading>
 
-            <List>
+          <List>
             {berbedas.map((berbeda, index) => (
-              <ListItem display="flex" alignItems="flex-start" fontWeight="medium" >
-                
-                  <HStack key={`berbeda-${index}`}>
-                    <Image w={20} mr="2" mt="1" alt="easeyourneeds" src={berbeda.icon} />
-                    <Text fontSize={{ base: '3vw', md: '1vw', lg:'xl' }} fontWeight={"black"} as="span" display="inline-block">
-                      {berbeda.name}
-                    </Text>
-                  </HStack>
-                
+              <ListItem key={`berbeda-${index}`} display="flex" alignItems="flex-start" fontWeight="medium">
+                <HStack>
+                  <Image w={20} mr="2" mt="1" alt="easeyourneeds" src={berbeda.icon} />
+                  <Text fontSize={{ base: '3vw', md: '1vw', lg:'xl' }} fontWeight={"black"} as="span" display="inline-block">
+                    {berbeda.name}
+                  </Text>
+                </HStack>
               </ListItem>
-              ))}
-            </List>
-          </VStack>
-        </Box>
+            ))}
+          </List>
+        </VStack>
+      </Box>
     </Box>
   );
 }
