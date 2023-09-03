220a65d1c9e88bb0fffcacc7fb7332ce0290c0d6
ananta <anantaristik@gmail.com>
2023-09-03 20:42:57 +0700

    contact transition upt

--- components/contactus/app.jsx
+++ components/contactus/app.jsx
@@ -1,11 +1,18 @@
-import { Box, Container, Text, HStack, Divider, Heading, Grid, GridItem, Icon, SlideFade } from "@chakra-ui/react";
+import { Box, Container, Text, HStack, Divider, Heading, Grid, GridItem, Icon, Center } from "@chakra-ui/react";
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 export default function ContactUs() {
 
-  const [ref, inViewport1] = useInView({rootMargin: '-20%'});
-  const [ref2, inViewport2] = useInView({rootMargin: '-10%'});
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+  const [ref2, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
   return (
     <Box>
@@ -32,35 +39,41 @@ export default function ContactUs() {
             w={{ base: 'auto', lg: 'auto'}}
             h={{ base: '20vh'}}
             p="2rem"
-            position="absolute"
             top="50%"
             left="50%"
+            position="absolute"
             transform="translate(-50%, -50%)"
             zIndex="1"
             color='white'
-
           >
             Contact Us
           </Heading>
+
         </Box>
       </Box>
 
-      <Container maxW="80vw" minW="80vw" pt={{ base: '15vh', md: '10vh' }}>
+      <Container maxW="80vw" minW="80vw" pt={{ base: '15vh', md: '10vh' }} ref={ref}>
         <HStack
           alignItems="initial"
           flexWrap={{ base: 'wrap', md: 'nowrap' }}
           justifyContent={{ base: 'center', md: 'space-between' }}
           spacing={{ base: '2rem', md: '0' }}
         >
-          <SlideFade in={inViewport1}>
-          <Box pr={{ base: '0', md: '10vw' }} w={{ base: '100%', md: '50vw' }} h={{ base: '30vh', md: 'auto' }} ref={ref}>
+         
+          <Box pr={{ base: '0', md: '10vw' }} w={{ base: '100%', md: '50vw' }} h={{ base: '30vh', md: 'auto' }} >
             <Divider mb="2vh" w="40%" alignItems="center" size="1vh" borderColor="black" variant="solid" orientation="horizontal" />
             <Heading fontSize={{ base:"4xl", lg:"4xl", md:"2xl" }}>Don&apos;t hesitate to contact us If you need more help</Heading>
           </Box>
-          </SlideFade>
+          
 
           <Box w={{ base: '100%', md: '50vw' }} fontSize="md" textAlign="justify">
-          <SlideFade in={inViewport2}> 
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={inView2 ? { opacity: 1, y: 0 } : { opacity: 0, y: -100 }}
+            exit={{ opacity: 0, y: -100 }}
+            
+            transition={{ duration: 2, ease: "easeOut" }}
+          >
             <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={10} ref={ref2}>
               <GridItem w="100%" h="20">
                 <Text fontWeight="bold" fontSize="25">
@@ -83,7 +96,7 @@ export default function ContactUs() {
                 </HStack>
               </GridItem>
             </Grid>
-            </SlideFade>
+            </motion.div>
           </Box>
         </HStack>
       </Container>
